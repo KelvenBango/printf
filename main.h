daf1141a41435add76423348926fc5150e754f67
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-14 19:19:22 +0300

    header file

---
+++ main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFSIZE 1024
+
+
+/* PRINTF */
+int _printf(const char *format, ...);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
