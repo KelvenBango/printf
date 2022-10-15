1f3d5de56a35275cb99f11b61de20fa55c68526c
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-15 13:13:37 +0300

    I'm not going anywhere

--- _printf.c
+++ _printf.c
@@ -1,6 +1,53 @@
 #include "main.h"
 
 /**
+ * spec_eng - calls the correct print function and returns a string
+ *
+ * @list: the va_list object
+ * @data: the parsed complete specifier
+ * @buffer: the main buffer
+ *
+ * Retrun: a string
+ */
+int spec_eng(va_list, spec_data_t *data, pf_buf_t *buffer)
+{
+	pf_buf_t *tmp;
+	int len = 0, i, j = 0;
+	specs_t specs[] = {
+		{'c', store_char},
+		{'s', store_string},
+		{'d', store_int},
+		{'i', store_int},
+		{'u', store_uint},
+		{'o', store_uoct},
+		{'x', store_lowuhex},
+		{'X', store_upuhex},
+		{'r', store_rev},
+		{'R', store_rot13},
+		{'b', store_binary},
+		{'S', store_strnop},
+		{'p', store_ptr},
+		{0, NULL}
+	};
+
+	for (i = 0; specs[i].spec; i++)
+	{
+		if (data->fmt_spec == specs[i].spec)
+		{
+			tmp = specs[i]. func(list, data);
+			if (tmp)
+			{
+				while (j < tmp->len)
+					pf_buf_t_add_char(buffer, tmp->buf[j++]);
+				len = tmp->len;
+				pf_buf_t_delete(tmp);
+			}
+		}
+	}
+	return (len);
+}
+	
+/**
  * _printf - entry point for our main function
  *
  * @format: the format string
