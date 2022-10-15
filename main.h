71879d4cfd8f51a3f1430d9dd2f0a009c6132950
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-15 12:43:15 +0300

    Header file

--- main.h
+++ main.h
@@ -9,6 +9,21 @@
 #define BUFSIZE 1024
 
 
+/**
+ * struct pf_buf - _printf buffer
+ *
+ * @buf: the buffer
+ * @len: the size of the string to add to main buffer
+ * @index: index of last char in main buffer
+ */
+typedef struct pf_buf
+{
+	char *buf;
+	int len;
+	int index;
+} pf_buf_t;
+
+
 /* PRINTF */
 int _printf(const char *format, ...);
 
