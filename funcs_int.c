806c8e2cb22b2f08a33865fafadbcd4c4acc1bd0
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-19 16:44:02 +0300

    containing int string

--- funcs_int.c
+++ funcs_int.c
@@ -1,15 +1,15 @@
-#include "main.h"
+include "main.h"
 
 /**
  * store_int - returns a temp buffer containing an int in string form
  *
- * @list: the va_list containing the element to print
+ * @list: th va_lis containing the elemenett to print
  * @data: the struct containing the specifier metadata
  *
- * Return:a pointer to a temp pf_buf_t
-*/
+ * Return: a pointer to a temp pf_buf_t struct
+ */
 
-pf_ strut cbuf_t *store_int(va_list list, spec_data_t *data)
+pf_buf_t *store_int(va_list list, spec_data_t *data)
 {
 	int num = va_arg(list, int);
 	pf_buf_t *tmp = NULL;
